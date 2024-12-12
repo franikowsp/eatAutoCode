4b2ffc80893fd7dc5e18739794c65ace64ae7d52
Philipp Franikowski <philipp.franikowski@gmail.com>
2024-12-12 20:07:10 +0100

    add bulk manual code insertion from JS

--- srcjs/modules/codeResponses.ts
+++ srcjs/modules/codeResponses.ts
@@ -1,4 +1,5 @@
 import { CodingScheme, ResponseStatusType } from "@iqb/responses";
+import insertManual from "../helpers/insertManual";
 import deparseJSON from "../helpers/deparseJSON";
 
 type UnitResponses = {
@@ -7,26 +8,41 @@ type UnitResponses = {
   value: any;
 };
 
+type ManualCodes = {
+  id: string;
+  status: ResponseStatusType;
+  code: number;
+};
+
 type UnitsArray = {
   responses: UnitResponses[];
+  manual: ManualCodes[];
 };
 
 export function codeResponses(params: {
   codingScheme: any;
   responses: UnitResponses[] | string;
+  manual: ManualCodes[] | string | null;
 }): UnitResponses[] {
-  const { codingScheme, responses } = params;
+  const { codingScheme, responses, manual } = params;
 
   // Parse responses and variableCodings if it's a JSON string
   const { variableCodings }: { variableCodings: any } =
     deparseJSON(codingScheme);
   const preparedScheme = new CodingScheme(variableCodings);
 
-  const responsesToCode: UnitResponses[] = deparseJSON(responses);
+  let responsesToCode: UnitResponses[] = deparseJSON(responses);
+  const manualToInsert: ManualCodes[] | null = manual && deparseJSON(manual);
+
+  if (manualToInsert !== null) {
+    responsesToCode = insertManual({
+      responses: responsesToCode,
+      manual: manualToInsert,
+    });
+  }
 
   // Create CodingScheme instance and code responses
   const coded = preparedScheme.code(responsesToCode);
-
   return coded;
 }
 
@@ -44,7 +60,20 @@ export function codeResponsesArray(params: {
   const responsesToCode: UnitsArray[] = deparseJSON(responses);
 
   const coded = responsesToCode.map((resp) => {
-    const responses: UnitResponses[] = deparseJSON(resp.responses);
+    let responses: UnitResponses[] = deparseJSON(resp.responses);
+
+    const manualToInsert: ManualCodes[] | null =
+      resp?.manual && deparseJSON(resp.manual);
+
+    if (manualToInsert !== null && manualToInsert !== undefined) {
+      responses = insertManual({
+        responses: responses,
+        manual: manualToInsert,
+      });
+
+      // Delete manual entry as it is not necessary anymore
+      delete resp.manual;
+    }
     resp.responses = preparedScheme.code(responses);
 
     return resp;
--- tests/testthat/test-code_responses.R
+++ tests/testthat/test-code_responses.R
@@ -30,6 +30,51 @@ test_that("correctly scores examplary data from JSON string", {
   expect_equal(responses_coded, comp)
 })
 
+comp <- data.frame(
+  id = c("b1", "b2", "b3"),
+  status = c("CODING_COMPLETE", "CODING_COMPLETE", "CODING_COMPLETE"),
+  value = c("TRUE", "true", "1"),
+  key = c("x1", NA, NA),
+  code = c(31, -99, -97),
+  score = c(1, 0, NA)
+)
+
+test_that("correctly adds and overwrites manual data", {
+  test_resp <- readr::read_file(test_path("code_responses/responses.txt")) %>%
+    jsonlite::parse_json() %>%
+    purrr::keep(function(x) x$id == "elementCodes") %>%
+    purrr::pluck(1, "content")
+
+  test_code <- list(list(id = "b1", status = "CODING_COMPLETE", code = 31, score = 1),
+                    list(id = "b2", status = "CODING_COMPLETE", code = -99, score = 0),
+                    list(id = "b3", status = "CODING_COMPLETE", code = -97, score = NA))
+
+  responses_coded <- code_responses(coding_scheme = test_cs,
+                                    responses = test_resp,
+                                    manual = test_code)
+
+  expect_equal(responses_coded, comp)
+})
+
+
+# test_that("correctly adds and overwrites manual data stored as JSON", {
+#   test_resp <- readr::read_file(test_path("code_responses/responses.txt")) %>%
+#     jsonlite::parse_json() %>%
+#     purrr::keep(function(x) x$id == "elementCodes") %>%
+#     purrr::pluck(1, "content")
+#
+#   test_code <- list(list(id = "b1", status = "CODING_COMPLETE", code = 31, score = 1),
+#                     list(id = "b2", status = "CODING_COMPLETE", code = -99, score = 0),
+#                     list(id = "b3", status = "CODING_COMPLETE", code = -97, score = NA))
+#
+#   test_code_json <- jsonlite::toJSON(test_code, auto_unbox = TRUE)
+#
+#   responses_coded <- code_responses(coding_scheme = test_cs,
+#                                     responses = test_resp,
+#                                     manual = test_code_json)
+#
+#   expect_equal(responses_coded, comp)
+# })
 # test_that("correctly aggregates missing data", {
   # test_resp <- readr::read_file(test_path("code_responses/responses.txt")) %>%
   #   jsonlite::parse_json() %>%
